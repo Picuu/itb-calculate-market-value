7f831c20dd4f3da18cf29a7b23fdb3ad868c82a5
ulises_castell <ulisescastell6@gmail.com>
2023-12-15 10:16:09 +0100

    add demo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 // Ulises Castell y Carlos Capó
 
-fun main() {
+fun calculatePrice (preu: Double, km: Double): Double {
+    val kmFinal = (0.00001 * km)
+    val preuFinal = kmFinal * preu
+    println(kmFinal)
+    println(preu - preuFinal)
+    return preuFinal
+}
 
+
+fun main () {
+    calculatePrice(preu = 73490.0, km = 12340.0)
 }
