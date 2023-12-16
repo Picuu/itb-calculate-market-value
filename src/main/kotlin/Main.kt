e0d899c60b2046583453c04ee67decd633d90af7
Picuu <ltpicuuu@gmail.com>
2023-12-16 01:03:43 +0100

    feat: create getFloat() function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
+import java.util.Scanner
+
 // Ulises Castell y Carlos Capó
 
 fun main () {
-    calculatePrice(73490.0f, 12340.0f)
+    println("Calcule el precio de su coche usado.")
+
+    val purchasePrice:Float = getFloat("Escriba el precio de mercado del vehículo: ")
+    val km:Float = getFloat("Escriba los kilómetros que tiene su coche: ")
+    val finalPrice:Float = calculatePrice(purchasePrice, km)
+
+    println("\nEl precio actual de su coche es de $finalPrice€")
 }
 
 fun calculatePrice (purchasePrice:Float, km:Float):Float {
@@ -9,4 +17,25 @@ fun calculatePrice (purchasePrice:Float, km:Float):Float {
     val wearPrice = carWear * purchasePrice
 
     return purchasePrice - wearPrice
+}
+
+fun getFloat(msg:String):Float {
+    val scanner:Scanner = Scanner(System.`in`)
+    print(msg)
+
+    var inputOk:Boolean = false
+    var float:Float = 0f
+    do {
+        if (scanner.hasNextFloat()) {
+            inputOk = true
+            float = scanner.nextFloat()
+            scanner.nextLine()
+        } else if (scanner.hasNextInt()) {
+            inputOk = true
+            float = scanner.nextInt().toFloat()
+            scanner.nextLine()
+        } else println("Escriba un número válido!")
+    } while (!inputOk)
+
+    return float
 }
