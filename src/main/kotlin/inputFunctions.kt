45d2f52c0dd9f7b4a207a26488d6942dca420058
ulisescastell <ulises.castell.7e7@itb.cat>
2023-12-17 13:00:25 +0100

    add new functions: getInt, getLong, getDouble

---
+++ src/main/kotlin/inputFunctions.kt
@@ -0,0 +1,83 @@
+import java.util.*
+
+fun getInt(msg:String, scanner: Scanner):Int {
+    print(msg)
+
+    var inputOk:Boolean
+    var int:Int = 0
+    do {
+        inputOk = scanner.hasNextInt()
+
+        if (inputOk) {
+            int = scanner.nextInt()
+            scanner.nextLine()
+        } else {
+            scanner.next()
+            println("Escriba un número válido!")
+        }
+    } while (!inputOk)
+
+    return int
+}
+
+
+fun getFloat(msg:String, scanner:Scanner):Float {
+    print(msg)
+
+    var inputOk:Boolean
+    var float:Float = 0f
+    do {
+        inputOk = scanner.hasNextFloat()
+
+        if (inputOk) {
+            float = scanner.nextFloat()
+            scanner.nextLine()
+        } else {
+            scanner.next()
+            println("Escriba un número válido!")
+        }
+    } while (!inputOk)
+
+    return float
+}
+
+
+fun getDouble(msg:String, scanner:Scanner):Double {
+    print(msg)
+
+    var inputOk:Boolean
+    var double:Double = 0.0
+    do {
+        inputOk = scanner.hasNextDouble()
+
+        if (inputOk) {
+            double = scanner.nextDouble()
+            scanner.nextLine()
+        } else {
+            scanner.next()
+            println("Escriba un número válido!")
+        }
+    } while (!inputOk)
+
+    return double
+}
+
+fun getLong(msg:String, scanner:Scanner):Long {
+    print(msg)
+
+    var inputOk:Boolean
+    var long:Long = 0
+    do {
+        inputOk = scanner.hasNextLong()
+
+        if (inputOk) {
+            long = scanner.nextLong()
+            scanner.nextLine()
+        } else {
+            scanner.next()
+            println("Escriba un número válido!")
+        }
+    } while (!inputOk)
+
+    return long
+}
